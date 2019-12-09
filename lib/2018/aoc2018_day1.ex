eaf99bc49cee3194c4c4722b56f8534f426d691f
nidzelskyy <igor.nidzelskyy@gmail.com>
2019-12-09 22:28:46 +0200

    add 2018 day 1 part 2

--- lib/2018/aoc2018_day1.ex
+++ lib/2018/aoc2018_day1.ex
@@ -13,7 +13,28 @@ defmodule Aoc2018Day1 do
   end
 
   def part_two() do
-    :second
+    info = parse_file()
+
+    Enum.reduce(1..150, %{sum: 0, list: [], catched_sum: nil}, fn x, prev ->
+      info
+      |> Enum.reduce(prev, fn x, %{sum: sum, list: list, catched_sum: cached_sum} ->
+          new_sum = sum + x
+          new_list = [new_sum | list]
+          case is_nil(cached_sum) do
+            true ->
+              new_cached =
+                case Enum.filter(list, fn exist_sum -> exist_sum == new_sum end) do
+                  [hd | _] -> hd
+                  [] -> nil
+                end
+              %{sum: new_sum, list: new_list, catched_sum: new_cached}
+            false ->
+              new_sum = sum + x
+              %{sum: new_sum, list: new_list, catched_sum: cached_sum}
+          end
+      end)
+    end)
+    |> Map.get(:catched_sum)
   end
 
   def parse_file() do
