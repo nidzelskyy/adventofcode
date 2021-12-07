d74671caa9d87a3e6131683ad8761b4d5be0684b
igorek <igor.nidzelskyy@ria.com>
2021-12-07 02:22:10 +0200

    2021_07-25 add modules templates

---
+++ lib/2021/aoc2021_day23.ex
@@ -0,0 +1,32 @@
+defmodule Aoc2021Day23 do
+  @moduledoc false
+
+  @quest_file "input23.txt"
+  @test_quest_file "input23_test.txt"
+
+  @needle 2021
+
+  #Aoc2021Day23.run(:test)
+  #Aoc2021Day23.run(:prod)
+  def run(mode \\ :test) do
+    IO.inspect("First part answer: #{inspect part_one(mode)}")
+    IO.inspect("Second part answer: #{inspect part_two(mode)}")
+  end
+
+  def part_one(mode) do
+    parse_file(mode)
+    :one
+  end
+
+  def part_two(mode) do
+    parse_file(mode)
+    :second
+  end
+
+  def parse_file(mode) do
+    File.stream!("inputs/#{@needle}/#{file_name(mode)}")
+  end
+
+  def file_name(:test), do: @test_quest_file
+  def file_name(_), do: @quest_file
+end
--- lib/aoc.ex
+++ lib/aoc.ex
@@ -5,15 +5,10 @@ defmodule Aoc do
 
   @doc """
   Hello world.
-
-  ## Examples
-
-      iex> Aoc.hello()
-      :world
-
+  Aoc.create_template()
   """
-  def create_template do
-    for(year <- 2015..2020, day <- 1..25, do: %{year: year, day: day, file: "aoc#{year}_day#{day}.ex", path: "lib/#{year}/"})
+  def create_template() do
+    for(year <- 2015..DateTime.utc_now.year(), day <- 1..25, do: %{year: year, day: day, file: "aoc#{year}_day#{day}.ex", path: "lib/#{year}/"})
     |> Enum.map(fn x ->
       case File.exists?(Map.get(x, :path)) do
         false -> File.mkdir_p(Map.get(x, :path))
@@ -26,22 +21,34 @@ defmodule Aoc do
 defmodule Aoc#{Map.get(x, :year)}Day#{Map.get(x, :day)} do
   @moduledoc false
 
-  def run() do
-    IO.inspect("First part answer: \#{inspect part_one()}")
-    IO.inspect("Second part answer: \#{inspect part_two()}")
+  @quest_file "input#{Map.get(x, :day)}.txt"
+  @test_quest_file "input#{Map.get(x, :day)}_test.txt"
+
+  @needle #{Map.get(x, :year)}
+
+  #Aoc#{Map.get(x, :year)}Day#{Map.get(x, :day)}.run(:test)
+  #Aoc#{Map.get(x, :year)}Day#{Map.get(x, :day)}.run(:prod)
+  def run(mode \\\\ :test) do
+    IO.inspect("First part answer: \#{inspect part_one(mode)}")
+    IO.inspect("Second part answer: \#{inspect part_two(mode)}")
   end
 
-  def part_one() do
+  def part_one(mode) do
+    parse_file(mode)
     :one
   end
 
-  def part_two() do
+  def part_two(mode) do
+    parse_file(mode)
     :second
   end
 
-  def parse_file() do
-    :parse_file
+  def parse_file(mode) do
+    File.stream!("inputs/\#{@needle}/\#{file_name(mode)}")
   end
+
+  def file_name(:test), do: @test_quest_file
+  def file_name(_), do: @quest_file
 end
 """
           File.write(Map.get(x, :path) <> Map.get(x, :file), template)
